10e2332f8d0add8785f3e733b2696f1b58e6b07c
Daniel Villarroel Torrez <dvtorrez@gmail.com>
2025-01-27 17:27:17 -0300

    Add description of new features

--- send_quote.py
+++ send_quote.py
@@ -15,6 +15,12 @@ def main():
     for chat_id in env_variables['CHAT_ID']:
         file_path = f"./{chat_id}-My Clippings.txt"
         quotes = parse_clippings(file_path)
+        # * feature 1
+        # TODO: Delete short version of the same quote
+        # * feature 2
+        # TODO: Create sqlite db to store data from quotes dictionary
+        # TODO: Every time this runs it should check if there is a new quote, if so it should add it to the database
+        # TODO: Random quote should be selected from the database
         message = select_random_quote(quotes)
         send_to_telegram(
                 message=message,
